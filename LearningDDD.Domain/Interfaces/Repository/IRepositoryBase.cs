4ccf628475fa1edae2d0f66ec03d70c9b9baf5c8
Weberth Fernandes <weberth.soares@aply.com.br>
2017-01-05 20:23:03 -0200

    IRepositoryBase.cs Added

---
+++ LearningDDD.Domain/Interfaces/Repository/IRepositoryBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace LearningDDD.Domain.Interfaces.Repository
+{
+    public interface IRepositoryBase<TEntity> where TEntity : class
+    {
+        void Add(TEntity obj);
+
+        void Update(TEntity obj);
+
+        void Remove(TEntity obj);
+
+        IEnumerable<TEntity> GetAll();
+
+        TEntity GetById(int id);
+
+        void Dispose();
+    }
+}
