1bdfde5641a2133d8e2767f401396af17e414781
Weberth Fernandes <weberth.soares@aply.com.br>
2017-01-06 14:21:07 -0200

    Changed fields Descriptions to test commit on github..

--- LearningDDD.Presentation/ViewModels/HabitViewModel.cs
+++ LearningDDD.Presentation/ViewModels/HabitViewModel.cs
@@ -13,8 +13,10 @@ namespace LearningDDD.Presentation.ViewModels
         [DisplayName("Descrição")]
         public string Description { get; set; }
 
+        [DisplayName("Ativo?")]
         public bool Active { get; set; }
         // public virtual IEquatable<HabitActivity> Notes { get; set; }
+        [DisplayName("Criado em")]
         public DateTime CreationDate { get; set; }
     }
 }
