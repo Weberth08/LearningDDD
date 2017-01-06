a8e86f25dcd3722935ef206219eb9be6567a4993
Weberth Fernandes <weberth.soares@aply.com.br>
2017-01-06 11:11:59 -0200

    Concrete repositories added.

---
+++ LearningDDD.Infrastructure.Data/Repository/HabitActivityRepository.cs
@@ -0,0 +1,9 @@
+﻿using LearningDDD.Domain.Entities;
+using LearningDDD.Domain.Interfaces.Repository;
+
+namespace LearningDDD.Infrastructure.Data.Repository
+{
+    class HabitActivityRepository : RepositoryBase<HabitActivity>, IHabitActivityRepository
+    {
+    }
+}
---
+++ LearningDDD.Infrastructure.Data/Repository/HabitRepository.cs
@@ -0,0 +1,10 @@
+﻿
+using LearningDDD.Domain.Entities;
+using LearningDDD.Domain.Interfaces.Repository;
+
+namespace LearningDDD.Infrastructure.Data.Repository
+{
+    public class HabitRepository : RepositoryBase<Habit>, IHabitRepository
+    {
+    }
+}
