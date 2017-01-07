95fa82a619d4425a8a5cbac3d44dba005b26340e
Weberth Fernandes <weberth.soares@aply.com.br>
2017-01-07 18:31:38 -0200

    Application Layer Added.

---
+++ LearningDDD.Application/AppServiceBase.cs
@@ -0,0 +1,46 @@
+﻿using LearningDDD.Application.Interfaces;
+using LearningDDD.Domain.Interfaces.Services;
+using System.Collections.Generic;
+
+namespace LearningDDD.Application
+{
+    public class AppServiceBase<Tentity> : IAppServiceBase<Tentity> where Tentity : class
+    {
+        private readonly IServiceBase<Tentity> _service;
+
+        public AppServiceBase(IServiceBase<Tentity> service)
+        {
+            _service = service;
+        }
+
+        public void Add(Tentity obj)
+        {
+            _service.Add(obj);
+        }
+
+        public void Dispose()
+        {
+            _service.Dispose();
+        }
+
+        public IEnumerable<Tentity> GetAll()
+        {
+            return _service.GetAll();
+        }
+
+        public Tentity GetById(int id)
+        {
+            return _service.GetById(id);
+        }
+
+        public void Remove(Tentity obj)
+        {
+            _service.Remove(obj);
+        }
+
+        public void Update(Tentity obj)
+        {
+            _service.Update(obj);
+        }
+    }
+}
---
+++ LearningDDD.Application/Interfaces/IAppServiceBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace LearningDDD.Application.Interfaces
+{
+    public interface IAppServiceBase<TEntity> where TEntity : class
+    {
+        void Add(TEntity obj);
+
+        void Update(TEntity obj);
+
+        void Remove(TEntity obj);
+
+        IEnumerable<TEntity> GetAll();
+
+        TEntity GetById(int id);
+
+        void Dispose();
+
+    }
+}
